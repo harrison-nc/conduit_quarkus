7f8de5ce2c3e53d4ee39600ba9636838e7a9d983
Harrison Chikwe <nhcdeveloper@gmail.com>
2022-03-28 22:44:24 +0100

    Fix: generated login id sequence

--- create.sql
+++ create.sql
@@ -1,16 +1,17 @@
 create schema if not exists conduit;
 
+create sequence conduit.login_id_seq as bigint
+    increment by 1
+    minvalue 100
+    start with 100;
+
 create table if not exists conduit.logins (
-    "id" bigint not null generated always as identity,
+    "id" bigint not null default nextval('login_id_seq'),
     email varchar(255) not null,
     password_hash varchar(255) not null,
     primary key(id)
 );
 
-create sequence if not exists conduit.login_id_seq as bigint
- increment by 1
- minvalue 100
- start with 100
- owned by conduit.logins."id";
+alter sequence conduit.login_id_seq owned by conduit.logins."id";
 
 alter table conduit.logins add constraint logins_email_unique unique(email);
--- drop.sql
+++ drop.sql
@@ -1,3 +1,2 @@
 drop table if exists conduit.logins;
--- drop sequence conduit.login_id_seq;
 drop schema if exists conduit;
